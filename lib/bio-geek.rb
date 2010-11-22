3a487274413f07d54a9cb54a2cb2eb1ba5a19723
Raoul J.P. Bonnal <ilpuccio.febo@gmail.com>
2010-11-22 19:25:17 +0100

    fix github repository name using bioruby-yourprojectname

--- lib/bio-geek.rb
+++ lib/bio-geek.rb
@@ -1,21 +1,53 @@
+#Overwrite Jeweler classes for properly configure a BioRuby Development Environment/Layout.
+
 class Jeweler
   class Generator 
+    module  GithubMixin
+      #class Jeweler::Generator::GithubMixin
+      def homepage
+        @homepage ||= "http://github.com/#{github_username}/#{github_repo_name}"
+      end
+      
+      #class Jeweler::Generator::GithubMixin      
+      def git_remote
+        @git_remote ||= "git@github.com:#{github_username}/#{github_repo_name}.git"
+      end
+    end
+    
       alias original_initialize initialize
       def initialize(options = {})
         original_initialize(options)
         development_dependencies  << ["bio", ">= 1.4.1"]
       end
-            
+          
+      alias original_project_name project_name  
+      def project_name
+        "bio-#{original_project_name}"
+      end
       def lib_filename
-         "bio-#{project_name}.rb"
+         "#{project_name}.rb"
       end
       
       def target_dir
-        "bioruby-#{project_name}"
-      end
+        project_name.gsub('bio','bioruby')
+      end      
+      alias github_repo_name target_dir
       
       def require_name
-        "bio-#{self.project_name}"
+        project_name
       end
+            
+      def create_and_push_repo
+        Net::HTTP.post_form URI.parse('http://github.com/api/v2/yaml/repos/create'),
+                                  'login' => github_username,
+                                  'token' => github_token,
+                                  'description' => summary,
+                                  'name' => github_repo_name
+        # TODO do a HEAD request to see when it's ready?
+        @repo.push('origin')
+      end
+      
+      
   end
 end
+
