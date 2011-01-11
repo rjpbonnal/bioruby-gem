42a34410666fc475cc75ad394f3dfa21e15f8085
Raoul J.P. Bonnal <ilpuccio.febo@gmail.com>
2011-01-11 10:01:19 +0100

    add executable bin with a template script on request --with-bin

--- lib/bio-gem/mod/jeweler.rb
+++ lib/bio-gem/mod/jeweler.rb
@@ -40,6 +40,31 @@ class Jeweler
     def bin_dir
       'bin'
     end
+    
+    def bin_name
+      "bio#{original_project_name}"
+    end
+
+    def render_template_generic(source, template_dir = template_dir_biogem)
+      template_contents = File.read(File.join(template_dir, source))
+      template          = ERB.new(template_contents, nil, '<>')
+
+      # squish extraneous whitespace from some of the conditionals
+      template.result(binding).gsub(/\n\n\n+/, "\n\n")
+    end
+
+    def output_template_in_target_generic(source, destination = source, template_dir = template_dir_biogem)
+      final_destination = File.join(target_dir, destination)
+      template_result   = render_template_generic(source, template_dir)
+
+      File.open(final_destination, 'w') {|file| file.write(template_result)}
+
+      $stdout.puts "\tcreate\t#{destination}"
+    end
+
+    def template_dir_biogem
+      File.join(File.dirname(__FILE__),'..', 'templates')
+    end
 
     alias original_create_files create_files
     # this is the defaul directory for storing library datasets
@@ -49,7 +74,12 @@ class Jeweler
       original_create_files
       mkdir_in_target test_data_dir if options[:biogem_test_data]
       mkdir_in_target db_dir if options[:biogem_db]
-      mkdir_in_target bin_dir if options[:biogem_bin]
+      if options[:biogem_bin] 
+        mkdir_in_target bin_dir
+        output_template_in_target_generic 'bin', File.join(bin_dir, bin_name)
+        # TODO: set the file as executable
+        File.chmod 0655, File.join(target_dir, bin_dir, bin_name)
+      end
     end
 
     def create_and_push_repo
